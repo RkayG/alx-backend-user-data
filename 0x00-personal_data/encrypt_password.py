ae24aca1d8afd63f7563b79ee9976d2d7095f06b
Your Name <you@example.com>
2024-05-29 13:48:28 +0000

    personal user data files

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
-"""This file deals with password validation
 """
+module that deals with
+encrypting passwords using
+bcrypt
+"""
+
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """This function hashes a password using bcrypt
-    hashpw and gensalt
-    Args:
-        password (str): the password to be hashed
-    Returns:
-        bytes: the hashed password
     """
-    result = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    return result
+    expects one string argument name password
+    and returns a salted, hashed password,
+    which is a byte string.
+    """
+
+    encrypt = password.encode()
+    hashed_pwd = bcrypt.hashpw(encrypt, bcrypt.gensalt(15))
+    return hashed_pwd
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks if a password is the same as the hashed password
-    Args:
-        hashed_password (bytes): the password which is the yardstick
-        password (str): the password to check
-    Returns:
-        bool: True or false depending on outcome
     """
-    return bcrypt.checkpw(password.encode(), hashed_password)
+     function that expects 2 arguments and returns a boolean.
+     Uses bcrypt to validate that the provided password matches
+     the hashed password.
+    """
+    valid = False
+    encrypt = password.encode()
+    if bcrypt.checkpw(encrypt, hashed_password):
+        valid = True
+    return valid
